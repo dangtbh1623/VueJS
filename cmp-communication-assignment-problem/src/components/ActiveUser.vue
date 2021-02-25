4bc9cb29e5157ade02e545b6898c5071a1a8d9ef
butri001 <buuhaidang.trinh001@stud.fh-dortmund.de>
2021-02-25 16:37:52 +0100

    addApp

---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box">
+    <h2>{{ person.name }}</h2>
+    <h3>{{ person.age }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: ["person"],
+};
+</script>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="box"><form>
+      <label>Name: <input name="name" v-model="enteredPerson.enteredName" /></label>
+      <label>Age: <input name="age" v-model="enteredPerson.enteredAge" /></label>
+      <button @click.prevent="addPerson">Save</button>
+    </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            enteredPerson: {
+                enteredName: '',
+                enteredAge: 0,
+            }
+        };
+    },
+    methods:{
+        addPerson(){
+            this.$emit('add',this.enteredPerson);
+        }
+    }
+};
+</script>
